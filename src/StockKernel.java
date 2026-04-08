79de0f6850cc3344e86f4dbd883b2236eaf5bbc5
Sagiv Tirosh <sagivtirosh793@gmail.com>
2026-04-08 08:45:16 -0400

    aa

--- src/StockKernel.java
+++ src/StockKernel.java
@@ -3,10 +3,7 @@ import components.standard.Standard;
 /**
  * Kernel interface for the Stock component.
  *
- * @mathmodel type StockKernel is (
- * ticker: string,
- * history: string of integer
- * )
+ * @mathmodel type StockKernel is ( ticker: string, history: string of integer )
  * @initially <pre>
  * (String symbol, int initialPrice):
  * requires: symbol is not empty and initialPrice > 0
@@ -17,7 +14,9 @@ public interface StockKernel extends Standard<Stock> {
 
     /**
      * Adds a new price point to the history.
-     * @param price the new price in cents
+     * 
+     * @param price
+     *            the new price in cents
      * @updates this
      * @requires price > 0
      * @ensures this.history = #this.history * <price>
@@ -26,6 +25,7 @@ public interface StockKernel extends Standard<Stock> {
 
     /**
      * Reports the most recent price.
+     * 
      * @return the current price in cents
      * @ensures currentPrice = last(this.history)
      */
@@ -33,7 +33,9 @@ public interface StockKernel extends Standard<Stock> {
 
     /**
      * Reports the price at a specific time step.
-     * @param timeStep the index in history
+     * 
+     * @param timeStep
+     *            the index in history
      * @requires 0 <= timeStep < |this.history|
      * @return the price at the time step
      * @ensures priceAt = this.history[timeStep]
@@ -42,6 +44,7 @@ public interface StockKernel extends Standard<Stock> {
 
     /**
      * Reports the number of recorded prices.
+     * 
      * @return total entries in history
      * @ensures historySize = |this.history|
      */
@@ -49,6 +52,7 @@ public interface StockKernel extends Standard<Stock> {
 
     /**
      * Reports the stock ticker symbol.
+     * 
      * @return the ticker symbol
      * @ensures tickerSymbol = this.ticker
      */
